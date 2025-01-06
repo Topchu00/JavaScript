2c04aa275c3afa5df098c97b71d188d4cb64c20c
Topchu00 <orozbekovtopchu@gmail.com>
2025-01-06 20:54:40 +0600

    lesson1

---
+++ index.js
@@ -0,0 +1,66 @@
+// Переменные в js 
+// let / const / var
+
+// let num1 = 12
+// const num2 = "pip"
+// var num3 = true
+
+// let num1 = 12
+// let $num_1
+
+// Названия наших переменных должно чётко обозначать что хранится в переменной
+// let name = "Morgan"
+// let surname = "Johnson"
+
+// console.log("Hello world!!")
+
+
+
+// let num = 2
+// const name = "Morgan"
+// var pi = 3.14
+
+// Значение переменной созданной с помощью let может быть перезаписана
+// Значение переменной созданной с помощью const не может быть перезаписана
+
+
+// Переменнная var имеет голобальную или функциональную область видимости, так же переменная устарела и не используется.
+
+// console.log(animal1);
+// var animal1 = "Dog"
+// console.log(animal1);
+
+// console.log(animal2);
+// const animal2 = "Dog"
+// console.log(animal2);
+
+// if(1 < 2) {
+//     var animal = "dog"
+//     const animal3 = "cat"
+// }
+
+// console.log(animal)
+// console.log(animal3);
+
+
+// Можно создавать переменным любые названия, но лучше всего на латинице 
+// let привет = "привет"
+// console.log(привет);
+
+// Запуск js файла с помощью node js в терминале
+// const animal = "monkey"
+
+// console.log(animal);
+
+// alert("Hello World")
+
+// const num = prompt("Введите число")
+// confirm("Are you sure?")
+
+// console.log(num);
+
+
+
+// const num = 1 + 1
+const num2 = 1 + '1'
+console.log(num2);
