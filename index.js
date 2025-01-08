6493048585099fb3d7bf8212739cb60bb04e744d
Topchu00 <orozbekovtopchu@gmail.com>
2025-01-08 20:49:42 +0600

    lesson2

--- index.js
+++ index.js
@@ -1,66 +1,112 @@
-// Переменные в js 
-// let / const / var
+// Типы данных в js
+// всего 8 типов данных:
+// 1) 7 примитивные типы данных:
+    // 1) number
+    // 2) string
+    // 3) boolean
+    // 4) null
+    // 5) undefined
+    // 6) BigInt
+    // 7) symbol
 
-// let num1 = 12
-// const num2 = "pip"
-// var num3 = true
+// 2) 1 ссылочный тип данных:
+    // object
+//_________________________________________________________________________________________________
 
-// let num1 = 12
-// let $num_1
+// Javascript - является динамически типизированным языком 
+// let num;
+// console.log(typeof num);
 
-// Названия наших переменных должно чётко обозначать что хранится в переменной
-// let name = "Morgan"
-// let surname = "Johnson"
+// num = 2;
+// console.log(typeof num);
 
-// console.log("Hello world!!")
-
-
-
-// let num = 2
-// const name = "Morgan"
-// var pi = 3.14
-
-// Значение переменной созданной с помощью let может быть перезаписана
-// Значение переменной созданной с помощью const не может быть перезаписана
+// num = ""
+// console.log(typeof num)
 
 
-// Переменнная var имеет голобальную или функциональную область видимости, так же переменная устарела и не используется.
+//BigInt - позволяет работать с большими числами выше и меньше 9007199254740991
+// console.log(BigInt(987654321987654) + BigInt(1));
+// console.log(BigInt(987654321987654) + BigInt(4));
 
-// console.log(animal1);
-// var animal1 = "Dog"
-// console.log(animal1);
+//Boolean - может быть только двумя значениями это (true / false) логический тип данных для выполнения условий  
 
-// console.log(animal2);
-// const animal2 = "Dog"
-// console.log(animal2);
+// 0/null/undefined/''/""/`` = false
+// console.log(Boolean());
 
-// if(1 < 2) {
-//     var animal = "dog"
-//     const animal3 = "cat"
+// console.log(50 < 1);
+
+// const age = prompt("Введите возраст: ")
+// if(age >= 18) {
+//     console.log("Добро пожаловать")
+// }else {
+//     console.log("Школьник")
+// }
+// console.log(age);
+
+
+// String - тип данных являющейся строкой заключённой в (двойные/одинарные/обратные ковычки)
+
+// let age = prompt("Введите возраст") 
+// let name = `Bob ${age} лет` особый вид строк позволяющий подставлять данные динамически в текст
+// let name = 'Bob'
+// let name = "Bob"
+
+// console.log(name);
+
+// let text = `ff
+// ff
+// ff`
+
+
+// Null && Undefined - Оба означают "пустоту", но отличие заключается в присвоении.
+
+// let num = null;
+
+// Object - Сложный тип данных который может хранить в себе коллекцию из большого количества данных.
+
+// let obj = {
+//     num: 56,
+//     text: 'fsdf',
+
 // }
 
-// console.log(animal)
-// console.log(animal3);
+
+//Symbol - тип данных уникальный идентификатор 
+
+// let num1 = '1'
+// let num2 = '1'
+
+// console.log('nums result =>', num1 == num2); //true
+
+// let text1 = Symbol('1')
+// let text2 = Symbol('1')
+
+// console.log('symbol result =>', text1 == text2); //false
+
+// let obj1 = {}
+// let obj2 = {}
+
+// console.log('object result =>', obj1 == obj2); //false
 
 
-// Можно создавать переменным любые названия, но лучше всего на латинице 
-// let привет = "привет"
-// console.log(привет);
+// Преобразование типов - Процесс при котором данные преобразовываются из одного типа в другой.
 
-// Запуск js файла с помощью node js в терминале
-// const animal = "monkey"
+// Явное преобразование типов:
 
-// console.log(animal);
-
-// alert("Hello World")
-
-// const num = prompt("Введите число")
-// confirm("Are you sure?")
-
+// let num = 54.752
 // console.log(num);
+// console.log(String(num).replace('.', ' '));
+// console.log(num.toString());
+
+// let text = "54.32"
+// let text = "hello"
+
+// console.log(Number(text));
+// console.log(+text)
 
 
+// Неявное преобразование типов:
 
-// const num = 1 + 1
-const num2 = 1 + '1'
-console.log(num2);
+// console.log(1 + '1');
+
+console.log('1' + [] + 0);
