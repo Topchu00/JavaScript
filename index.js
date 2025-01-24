ded365bb9705275d0451c7189300ca63d71747c2
Topchu00 <orozekovtopchu@gmail.com>
2025-01-24 17:57:05 +0300

    lesson9

--- index.js
+++ index.js
@@ -1,160 +1,202 @@
-// 1. Выведите столбец четных чисел в промежутке от 0 до 100.
+// Копирование объектов
 
-// for(let i = 0; i <= 100; i++) {
-//     if(i % 2 == 0) {
-//         console.log(i);
-        
-//     }
+// const name = 'Azim'
+// const cloneName = name
+
+// console.log('cloneName === name', cloneName === name);
+
+// const user = {
+//     name: "Azat",
+//     age: 25,
+//     password: '123456'
+// }
+
+// const cloneUser = user
+// cloneUser.name = "Madina"
+
+// console.log('cloneuser', cloneUser);
+// console.log('user', user);
+
+// Первый способ копирования объектов через цикл for in 
+
+// const cloneUser = {}
+
+// for(let key in user) {
+//     cloneUser[key] = user[key]
+//     // console.log(cloneUser);
+// }
+
+// cloneUser.email = 'asdfsfsdf'
+
+// console.log('cloneuser', cloneUser);
+// console.log('user', user);
+
+
+// Второй способ копирования объектов через метод assign
+
+// const user = {
+//     name: "Azat",
+//     age: 25,
+//     password: '123456'
+// }
+
+// const userId = {
+//     id: 12
+// }
+
+// const userStatus = {
+//     status: 'mentor'
+// }
+
+// // Object.assign(user, userId, userStatus)
+
+// const newUser = {
+//     name: 'Azim'
+// }
+// Object.assign(newUser, user, userStatus, userId)
+
+// console.log('newUser', newUser);
+
+
+// Третий способ копирования объектов через спредоператор
+
+// const user = {
+//     name: "Azat",
+//     age: 25,
+//     password: '123456'
+// }
+
+// const userId = {
+//     id: 21
+// }
+
+// const newUser = {...user, ... userId}
+// // const newUser = {...user.age}
+// newUser.name = "Azim"
+// console.log('user', user);
+// console.log('newUser', newUser);
+
+
+
+// НЕЛЬЗЯ ТАК ДЕЛАТЬ!!
+// const user = {
+//     name: "Azat",
+//     age: 25,
+//     password: '123456'
+// }
+
+// const newUser = user
+
+// newUser.name = 'Madina'
+
+// console.log('user', user);
+// console.log('newUser', newUser);
+
+
+
+
+
+
+// this
+
+// a = 'Hello world'
+
+// console.log(this.alert("hello world"));
+
+// console.log(this === window);
+// name = 'Rustam'
+// function hello() {
+//     console.log(this.name);
     
 // }
 
-
-
-// 2.  Дан массив с элементами [2, 5, 9, 15, 0, 4] С помощью цикла for и оператора if выведите на экран столбец тех элементов массива, которые больше 3-х, но меньше 10.
-
-// const arr = [2, 5, 9, 15, 0, 4]
-
-// for(let i = 0; i <= arr.length; i++) {
-//     if(arr[i] > 3 && arr[i] < 10) {
-//         console.log(arr[i]);
-        
-//     }
+// const person = {
+//     name: "Azim",
+//     sayHello : hello
 // }
 
-// 3. Дан объект с числовыми значениями. Найдите сумму всех значений. Используйте цикл for...in для перебора свойств объекта.
-
-// const obj = {
-//     num1: 3, 
-//     num2: 5, 
-//     num3: 9,
-//     num4: 13,
-//     num5: 4,
-//     num6: 7
-// }
-// let sum = 0
-
-// for(let i in obj) {
-//     sum += obj[i]
+// const persontwo = {
+//     name: "Madina",
+//     sayHello : hello
 // }
 
-// console.log(sum);
+// person.sayHello()
+// persontwo.sayHello()
+// hello()
 
 
-// 4. Дан массив с числами. Числа могут быть положительными и отрицательными. Найдите сумму положительных элементов массива.
 
-// const numbers = [-2, 5, 1, -5, -1, 1, 3, 4, -1];
-// let sum = 0 
-
-// for(let i = 0; i < numbers.length; i++) {
-//     if(numbers[i] > 0) {
-//         sum += numbers[i];
-//     }
+// const sayHello = () => {
+//     console.log("Hello world");
 // }
 
-// console.log(sum);
 
+// sayHelloFunc()
 
-// 5. Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
-// let Dayweek = prompt("Введите цифру от 1 до 7")
-
-// function Calendar(day) {
-//     if(day == 1) {
-//         return "Понедельник"
-//     }else if (day == 2) {
-//         return"Вторник";
-        
-//     }else if (day == 3) {
-//         return "Среда";
-        
-//     }else if (day == 4) {
-//         return "Четверг";
-        
-//     }else if (day == 5) {
-//         return "Пятница";
-        
-//     }else if (day == 6) {
-//         return "Суббота";
-        
-//     }else if (day == 7) {
-//         return "Воскресенье";
-        
-//     }
-// }
-
-// alert(Calendar(Dayweek))
-
-
-// 6. С помощью цикла for сформируйте строку '987654321' и запишите ее в переменную str
-
-// let str = ''
-// for(let i = 9; i >= 1; i--) {
-//     str += i
+// function sayHelloFunc() {
+//     console.log("Hello world");
     
 // }
 
-// console.log(str);
+// sayHelloFunc()
 
 
-// 7. С помощью цикла for сформируйте строку '-1-2-3-4-5-6-7-8-9-' и запишите ее в переменную str.
+// Передать контекст
 
-// let str = ''
-
-// for(let i = 1; i < 10; i++) {
-//     str += -${i}
+// function sayHello(email, gender) {
+//     console.group(`${this.name} Info`)
+//     console.log(`Hello ${this.name}. age: ${this.age}, password: ${this.password}`);
+//     console.log('email: ' + email);
+//     console.log('gender: ' + gender);
+//     console.groupEnd();
+    
 // }
 
-// str += '-'
-
-// console.log(str);
-
-
-// 8. Заполните массив следующим образом: в первый элемент запишите '1', во второй '22', в третий '333' и так далее.
-
-// let n = 8; // Размер массива (можно изменить на нужное значение)
-// let arr = [];
-
-// for (let i = 1; i <= n; i++) {
-//     let str = "";
-//     for (let j = 0; j < i; j++) {
-//         str += i;
-//     }
-//     arr[i - 1] = str; // Прямое присваивание элемента массива
+// const userAzat = {
+//     name: 'Azat',
+//     age: 23,
+//     password: '1234',
+//     // hello: sayHello
 // }
 
-// console.log(arr);
-
-
-// 9. Найдите сумму всех чисел от 1 до 100
-
-// let sum = 0
-// for(let i = 0; i <= 100; i++) {
-//     sum += i
+// const userAzim = {
+//     name: "Azim",
+//     age: 25,
+//     password: '123456',
+//     hello: sayHello  // bind(userAzat)        // this не является userAzim, а userAzat
 // }
-// console.log(sum);
+
+// userAzim.hello.bind(userAzat, 'asdasd', 'asdasd')() 
+// userAzim.hello.call(userAzat, 'asdasd', 'asdasd')
+// userAzim.hello.apply(userAzim, ['asdasd', 'asdasd'])
 
 
-// 10. Создайте функцию countProperties(obj), которая принимает объект и возвращает количество его собственных свойств.
+// userAzim.hello.call(userAzat)
 
-// function countProperties(obj) {
-//     let count = 0;
-//     for (let key in obj) {
-//       if (obj[key] !== undefined || key in obj) {
-//         count++;
-//       }
-//     }
-//     return count;
-//   }
-  
+// userAzim.hello()
+// userAzat.hello()
 
-// const numericValues = {
-//     age: 20,
-//     height: 175,
-//     weight: 70,
-//     salary: 50000,
-//     experienceYears: 3,
-//     projectsCompleted: 15,
-//     rating: 4.8,
-//     tasksPerDay: 10
-// };
 
-// console.log(countProperties(numericValues));
+
+
+
+// Конструктор, оператор "new"
+
+// function User(name) {
+//     // this {}
+//     this.name = name
+//     this.status = 'student'
+//     this.group = 'js-15'
+
+//     // return {} 
+// }
+
+// // const studentRamazan = new User('Ramazan')
+
+// const students = ['Ramazan', 'Topchubai', 'Aisha', 'Rustam']
+// const studentsObjArr = []
+// for(let i = 0; i < students.length; i++) {
+//     studentsObjArr.push(new User(students[i]))
+// }
+
+// console.log(studentsObjArr);
